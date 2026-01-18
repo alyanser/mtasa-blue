626423b6253ac85d403cb5c30eba7f2a36c152ae
Aly Anser <alyystaz@gmail.com>
2026-01-18 15:04:43 +0500

    Linux build using diferent bin & build directories

--- utils/buildactions/install_data.lua
+++ utils/buildactions/install_data.lua
@@ -3,29 +3,20 @@ require 'utils'
 premake.modules.install_data = {}
 
 -- Config variables
-local BIN_DIR = "Bin"
+local WIN_BIN_DIR = "Bin"
+local LINUX_BIN_DIR = "linux-bin"
 local DATA_DIR = "Shared/data/MTA San Andreas"
-local BASE_URL = "https://mirror-cdn.multitheftauto.com/bdata/1.6/"
-
-local NET_PATH_X86_WIN   = BASE_URL .. "net.dll"
-local NET_PATH_X64_WIN   = BASE_URL .. "net_64.dll"
-local NET_PATH_ARM64_WIN = BASE_URL .. "net_arm64.dll"
-local NETC_PATH_WIN      = BASE_URL .. "netc.dll"
-
-local NET_PATH_X86_LINUX   = BASE_URL .. "net.so"
-local NET_PATH_X64_LINUX   = BASE_URL .. "net_64.so"
-local NET_PATH_ARM_LINUX   = BASE_URL .. "net_arm.so"
-local NET_PATH_ARM64_LINUX = BASE_URL .. "net_arm64.so"
-
-local NET_PATH_X64_MACOS = BASE_URL .. "net.dylib"
 
 newaction {
 	trigger = "install_data",
 	description = "Installs data files",
 
 	execute = function()
+		local BIN_DIR = os.host() == "windows" and WIN_BIN_DIR or LINUX_BIN_DIR
+
 		-- Make Bin directory if not exists
 		local success, message = os.mkdir(BIN_DIR)
+
 		if not success then
 			errormsg("ERROR: Couldn't create Bin directory", "\n"..message)
 			os.exit(1)
@@ -73,87 +64,18 @@ newaction {
 		end
 
 		if os.host() == "windows" then
-			local success = http.download_print_errors(NET_PATH_X86_WIN, BIN_DIR.."/server/net.dll")
-			success = success and http.download_print_errors(NET_PATH_X64_WIN, BIN_DIR.."/server/x64/net.dll")
-			success = success and http.download_print_errors(NET_PATH_ARM64_WIN, BIN_DIR.."/server/arm64/net.dll")
-			success = success and http.download_print_errors(NETC_PATH_WIN, BIN_DIR.."/MTA/netc.dll")
-			
-			-- A download failed
+			local success = os.copyfile(BIN_DIR.."/../net/netc.dll", BIN_DIR.."/mta/netc.dll")
+			success = success and os.copyfile(BIN_DIR.."/../net/net.dll", BIN_DIR.."/server/net.dll")
+
 			if not success then
-				os.exit(1)
-				return
-			end
-
-			if not os.copyfile(BIN_DIR.."/MTA/netc.dll", BIN_DIR.."/MTA/netc_d.dll") then
-				errormsg("ERROR: Could not copy netc.dll")
-				os.exit(1)
-				return
-			end
-
-			if not os.copyfile(BIN_DIR.."/server/net.dll", BIN_DIR.."/server/net_d.dll") then
-				errormsg("ERROR: Could not copy server/net.dll")
-				os.exit(1)
-				return
-			end
-
-			if not os.copyfile(BIN_DIR.."/server/x64/net.dll", BIN_DIR.."/server/x64/net_d.dll") then
-				errormsg("ERROR: Could not copy server/x64/net.dll")
-				os.exit(1)
-				return
-			end
-
-			if not os.copyfile(BIN_DIR.."/server/arm64/net.dll", BIN_DIR.."/server/arm64/net_d.dll") then
-				errormsg("ERROR: Could not copy server/arm64/net.dll")
-				os.exit(1)
-				return
-			end
-		elseif os.host() == "macosx" then
-			local c = string.char(27)
-			print(string.format("Listen, I ain't leaving here till you tell me where the macOS net builds are.\n       " ..
-				" So come on bub, for old times' sake, huh?\n\t%s[45m%s[37mDid you just call me... BLOB?%s[0m\n", c,c,c,c))
-
-			if not http.download_print_errors(NET_PATH_X64_MACOS, BIN_DIR.."/server/x64/net.dylib") then
-				os.exit(1)
-				return
-			end
-
-			if not os.copyfile(BIN_DIR.."/server/x64/net.dylib", BIN_DIR.."/server/x64/net_d.dylib") then
-				errormsg("ERROR: Could not copy server/x64/net.dylib")
 				os.exit(1)
 				return
 			end
 		else
-			local success = http.download_print_errors(NET_PATH_X86_LINUX, BIN_DIR.."/server/net.so")
-			success = success and http.download_print_errors(NET_PATH_X64_LINUX, BIN_DIR.."/server/x64/net.so")
-			success = success and http.download_print_errors(NET_PATH_ARM_LINUX, BIN_DIR.."/server/arm/net.so")
-			success = success and http.download_print_errors(NET_PATH_ARM64_LINUX, BIN_DIR.."/server/arm64/net.so")
+			local success = os.copyfile(BIN_DIR.."/../net/net.so", BIN_DIR.."/server/net.so")
+			local success = os.copyfile(BIN_DIR.."/../net/net.so", BIN_DIR.."/server/x64/net.so")
 
-			-- A download failed
 			if not success then
-				os.exit(1)
-				return
-			end
-
-			if not os.copyfile(BIN_DIR.."/server/net.so", BIN_DIR.."/server/net_d.so") then
-				errormsg("ERROR: Could not copy server/net.so")
-				os.exit(1)
-				return
-			end
-
-			if not os.copyfile(BIN_DIR.."/server/x64/net.so", BIN_DIR.."/server/x64/net_d.so") then
-				errormsg("ERROR: Could not copy server/x64/net.so")
-				os.exit(1)
-				return
-			end
-
-			if not os.copyfile(BIN_DIR.."/server/arm/net.so", BIN_DIR.."/server/arm/net_d.so") then
-				errormsg("ERROR: Could not copy server/arm/net.so")
-				os.exit(1)
-				return
-			end
-
-			if not os.copyfile(BIN_DIR.."/server/arm64/net.so", BIN_DIR.."/server/arm64/net_d.so") then
-				errormsg("ERROR: Could not copy server/arm64/net.so")
 				os.exit(1)
 				return
 			end
